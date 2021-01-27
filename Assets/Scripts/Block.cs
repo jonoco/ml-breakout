2a1c304e76094c528bc0e27273d4ea5a7dbc1849
richertf@oregonstate.edu <richertf@oregonstate.edu>
2021-01-26 20:51:52 -0500

    testing a commit

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -14,6 +14,7 @@ public class Block : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("THIS IS A TEST");
         playerSupervisor = FindObjectOfType<PlayerSupervisor>();
     }
 
