c6f49d301ff68b936a1d4f1c91b2657039d4ccdd
richertf@oregonstate.edu <richertf@oregonstate.edu>
2021-01-31 20:13:08 -0500

    Updated PlayerSupervisor to increase game speed given criteria player defines in Unity. Player can define the starting game speed, how frequently the game speed increases (ever x seconds), and by what % the game speed increases when it does. This was an easier way of handling the ball velocity increasing over time.

--- Assets/Scripts/PlayerSupervisor.cs
+++ Assets/Scripts/PlayerSupervisor.cs
@@ -7,10 +7,28 @@ public class PlayerSupervisor : MonoBehaviour
     [SerializeField] Ball ball;
     [SerializeField] GameManager gameManager;
     [SerializeField] int activeBlocks;
-    
+
+    // Used for increasing ball velocity throughout game
+    [Range(0.1f, 5f)]
+    [SerializeField] float currentGameSpeed = 1f; 
+
+    // Increase the game speed every x # of seconds defined here
+    [Range(0.1f,15)]
+    [SerializeField] float speedIncreaseIncrementInSeconds = 5;
+
+    // Percentage the game speed increases after each increment defined in speedIncreaseIncrementInSeconds
+    [Range(0,15)]
+    [SerializeField] int speedIncreasePctEachIncrement = 1;
+
+    // To keep track of when the last speed increase was to determine if a new speed incr. is needed
+    private float previousIncreaseTimeInSeconds = 0;
+
     // Frannie's Level Items
     private RandomBlockCreator randomBlockCreator;
     private int points = 0;
+
+    // storing starting number of blocks for game speed increase throughout game
+    private int numStartingBlocks;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +43,15 @@ public class PlayerSupervisor : MonoBehaviour
     
         activeBlocks = FindObjectsOfType<Block>().Length;
         ball = FindObjectOfType<Ball>();
+
+        // game speed
+        SetGameSpeed();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IncreaseGameSpeed();
     }
 
     public void StartGame()
@@ -47,6 +74,21 @@ public class PlayerSupervisor : MonoBehaviour
         Destroy(ball.gameObject);
         
         gameManager.LoseGame();
+    }
+
+    public void SetGameSpeed()
+    {
+        Time.timeScale = currentGameSpeed;
+    }
+
+    public void IncreaseGameSpeed()
+    {
+        if(Time.time - previousIncreaseTimeInSeconds > speedIncreaseIncrementInSeconds)
+        {
+            previousIncreaseTimeInSeconds = Time.time;
+            currentGameSpeed *= (1 + (float)speedIncreasePctEachIncrement/100);
+            SetGameSpeed();
+        }        
     }
 
     public void BlockDestroyed(int pointValue)
