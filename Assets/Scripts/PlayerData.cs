936b9d303966736204e7bcdc5a0f07dd20063389
Frannie <frannie.richert@gmail.com>
2021-02-10 14:33:11 -0500

    added more data to track in PlayerData class

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -7,4 +7,28 @@ public class PlayerData : ScriptableObject
 {
     public int points;
     public string gameResult; 
+
+  // NOTE: All of these values RELY on the fact that this object
+    // is NEVER reset until one stops Unity with the play button.
+
+    [Tooltip("Number of games played in a single performance-tracking session")]
+    public int numGames;  
+
+    [Tooltip("List of all game scores, by individual game, len should match numGames")]
+    public List<int> gameScoresList;
+
+    [Tooltip("List of number of blocks broken, by individual game, len should match numGames")]
+    public List<int> blocksBrokenList;
+
+    [Tooltip("true=win, false=lose', by individual game, len should match numGames")]
+    public List<bool> gameWinStatusList;
+
+    void Awake()
+    {
+        gameScoresList = new List<int>();
+        blocksBrokenList = new List<int>();
+        gameWinStatusList = new List<bool>();
+    }
+
 }
+
