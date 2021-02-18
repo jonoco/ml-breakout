46d452c8cbd453d2ac3c339fe70fd0f734772041
matzingc <52265748+matzingc@users.noreply.github.com>
2021-02-17 19:00:08 -0600

    Create CM_01 Script
    Create CM_01 Script

---
+++ Assets/Scripts/Agents/CM_01.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CM_01 : PlayerAgent
+{
+    void Awake()
+    {
+        timeLimit = 0f;
+        blockReward = .5f;
+        loseBallPenalty = -10f;
+        paddleReward = .1f;
+        timeoutPenalty = 0f;
+    }
+}
