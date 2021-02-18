998efaaccbc64cf530d05a77c8cc0ab5aa724973
matzingc <52265748+matzingc@users.noreply.github.com>
2021-02-17 21:07:48 -0600

    Revise CM_04
    Revise CM_04

---
+++ Assets/Scripts/Agents/CM_04.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class CM_04 : PlayerAgent
+{
+    void Awake()
+    {
+        timeLimit = 120.0f;
+        blockReward = 5.0f;
+        loseBallPenalty = -50.0f;
+        paddleReward = 0.1f;
+        timeoutPenalty = 0.0f;
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        base.CollectObservations(sensor);
+
+        Block[] blocks = playerSupervisor.GetComponentsInChildren<Block>();
+        foreach (Block block in blocks)
+        {
+            sensor.AddObservation(block.transform.localPosition.x / playerSupervisor.instanceWidth);
+            sensor.AddObservation(block.transform.localPosition.y / playerSupervisor.instanceHeight);
+        }
+
+    }
+}
