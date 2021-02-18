a55e3c1b3fdc33b06faf935aad13dca33390ee6c
matzingc <52265748+matzingc@users.noreply.github.com>
2021-02-17 19:20:42 -0600

    Create Agents CM_01-CM_03
    Create Agents CM_01-CM_03

--- Assets/Scripts/Agents/CM_02.cs
+++ Assets/Scripts/Agents/CM_02.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CM_01 : PlayerAgent
+public class CM_02 : PlayerAgent
 {
     void Awake()
     {
         timeLimit = 120.0f;
-        blockReward = 0.05f;
-        loseBallPenalty = 0.0f;
-        paddleReward = 0.0f;
+        blockReward = 0.5f;
+        loseBallPenalty = -0.5f;
+        paddleReward = 0.001f;
         timeoutPenalty = 0.0f;
     }
 }
--- Assets/Scripts/Agents/CM_03.cs
+++ Assets/Scripts/Agents/CM_03.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CM_01 : PlayerAgent
+public class CM_03 : PlayerAgent
 {
     void Awake()
     {
         timeLimit = 120.0f;
-        blockReward = 0.05f;
-        loseBallPenalty = 0.0f;
-        paddleReward = 0.0f;
+        blockReward = 5.0f;
+        loseBallPenalty = -50.0f;
+        paddleReward = 0.1f;
         timeoutPenalty = 0.0f;
     }
 }
