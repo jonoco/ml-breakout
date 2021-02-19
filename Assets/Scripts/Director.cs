458dbfaf7678d6720cf6613e7917b1c6624df064
Joshua <joshua.n.cox@gmail.com>
2021-02-19 14:10:00 -0800

    Added intro video and menu title

---
+++ Assets/Scripts/Director.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Director : MonoBehaviour
+{
+    public SceneLoader sceneLoader;
+    public string startScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+
+        GetComponent<VideoPlayer>().loopPointReached += FinishedVideo;
+    }
+
+    void FinishedVideo(VideoPlayer vp)
+    {
+        sceneLoader.LoadScene(startScene);
+    }
+}
